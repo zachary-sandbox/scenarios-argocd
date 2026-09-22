589e977b5493a9e1d9a8714602d586c45cc06f4b
Zachary Lee <zachary404@163.com>
2026-09-22 22:30:06 +0800

    fix: update phase1 - verify.sh

--- 01-basic-concept-and-lab-prepare/install-argocd/assets/install_argocd.sh
+++ 01-basic-concept-and-lab-prepare/install-argocd/assets/install_argocd.sh
@@ -24,12 +24,7 @@ ADMIN_PWD=$(kubectl get secret argocd-initial-admin-secret \
 -o jsonpath="{.data.password}" | base64 -d)
 
 # ArgoCD Login Url
-ARGOCD_URL=$(echo "https://cdac3975fe6801fc-1-PORT.papa.r.killercoda.com" | sed 's/PORT/30080/')
-
-echo "ArgoCD Namespace: argocd"
-echo "Url: ${ARGOCD_URL}"
-echo "Username: admin"
-echo "Initial Admin Password: ${ADMIN_PWD}"
+ARGOCD_URL=$(cat /etc/killercoda/host | sed 's/PORT/30080/')
 
 # Optional: save password to local file
 echo "${ADMIN_PWD}" > ./argocd_admin_password.txt
@@ -38,10 +33,14 @@ echo -e "\nPassword also saved to local file: ./argocd_admin_password.txt"
 echo -e "\n===== Step5 Start background port‑forward for Argo‑CD UI ====="
 kubectl port-forward svc/argocd-server -n argocd 30080:80 >/dev/null 2>&1 &
 PF_PID=$!
+
+echo "ArgoCD Namespace: argocd"
+echo "Argo‑CD Web UI URL: ${ARGOCD_URL}"
+echo "Username: admin"
+echo "Initial Admin Password: ${ADMIN_PWD}"
 echo "Port‑forward background PID: ${PF_PID}"
-echo "Argo‑CD Web UI URL: https://127.0.0.1:8080"
 echo "To stop port‑forward manually, execute: kill ${PF_PID}"
 
 # Optional: auto‑login using argocd CLI
-echo -e "\n===== Step6 Try argocd CLI login ====="
-argocd login "$ARGOCD_URL" --username admin --password 85JpZdUMOUWMzcJ4 --insecure --grpc-web
+echo -e "\n===== Step6 (Optional) Try argocd CLI login ====="
+echo "argocd login "$ARGOCD_URL" --username admin --password ${ADMIN_PWD} --insecure --grpc-web"
